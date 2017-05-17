7ba10b738a71926876b7b46abcebb58ade5979ce
vborshchov <victor.borshchov@gmail.com>
2017-05-17 21:56:01 +0300

    #1.1.12 add default true value for colorizing output

--- config.rb
+++ config.rb
@@ -30,6 +30,7 @@ class Config
       result = exists? ? data : {}
       result['mode'] = result['mode'] || 'hotfix'
       result['locale'] = result['locale'] || 'en'
+      result['colorize'] = result['colorize'] || true
       result
     end
 
