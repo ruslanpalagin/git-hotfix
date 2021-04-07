e430fe874ec2320d9f60a68c99ff8c4c68878576
Ruslan <ruslan.palagin.ck@gmail.com>
2021-04-07 16:01:59 +0300

    [HF] #2.1.17: show only last 20 for hf my

--- actions/my_action.rb
+++ actions/my_action.rb
@@ -2,8 +2,11 @@
 class MyAction
   class << self
     def call args, options
-      result = `git for-each-ref --sort=-committerdate refs/heads/`.gsub("refs/heads/", "")
-      puts result
+      result = `git for-each-ref --sort=-committerdate refs/heads/`
+                   .gsub(/(.*)refs\/heads\//, "")
+                   .split("\n")
+      puts result[0..20]
+
       exit
     end
 
